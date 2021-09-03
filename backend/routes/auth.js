5b21f098858566481beae830abce385015704ad0
CodeWithHarry <48705673+CodeWithHarry@users.noreply.github.com>
2021-09-03 15:49:55 +0530

    Video 50 Completed

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -46,8 +46,48 @@ router.post('/createuser', [
     
   } catch (error) {
     console.error(error.message);
-    res.status(500).send("Some Error occured");
+    res.status(500).send("Internal Server Error");
   }
 })
 
+
+// Authenticate a User using: POST "/api/auth/login". No login required
+router.post('/login', [ 
+  body('email', 'Enter a valid email').isEmail(), 
+  body('password', 'Password cannot be blank').exists(), 
+], async (req, res) => {
+
+  // If there are errors, return Bad request and the errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const {email, password} = req.body;
+  try {
+    let user = await User.findOne({email});
+    if(!user){
+      return res.status(400).json({error: "Please try to login with correct credentials"});
+    }
+
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if(!passwordCompare){
+      return res.status(400).json({error: "Please try to login with correct credentials"});
+    }
+
+    const data = {
+      user:{
+        id: user.id
+      }
+    }
+    const authtoken = jwt.sign(data, JWT_SECRET);
+    res.json({authtoken})
+
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+
+
+})
 module.exports = router
