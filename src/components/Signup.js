739de41eb0feb3be77cb6ea6918a2a210077598a
Anurag Singh <72223953+anurag-singh2001@users.noreply.github.com>
2021-10-01 00:41:37 +0530

    Update Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -3,7 +3,35 @@ import React from 'react'
 const Signup = () => {
     return (
         <div>
-            I am signup
+           <div className="container register-form">
+            <div className="form">
+                <div className="note">
+                    <p>This is a simpleRegister Form made using Boostrap.</p>
+                </div>
+
+                <div className="form-content">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <div className="form-group">
+                                <input type="text" className="form-control" placeholder="Your Name *" value=""/>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" className="form-control" placeholder="Phone Number *" value=""/>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <input type="text" className="form-control" placeholder="Your Password *" value=""/>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" className="form-control" placeholder="Confirm Password *" value=""/>
+                            </div>
+                        </div>
+                    </div>
+                    <button type="button" class=Name"btnSubmit">Submit</button>
+                </div>
+            </div>
+        </div>
         </div>
     )
 }
