289f16e2b7df7f2230c3ac3e1f52dd11ec1402e3
mohib <itsmohibuddin@gmail.com>
2023-10-21 07:59:40 +0530

    fix: fixing the mongodb connection issue

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+const mongoURI = "mongodb://127.0.0.1:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
 
 const connectToMongo = ()=>{
-    mongoose.connect(mongoURI, ()=>{
-        console.log("Connected to Mongo Successfully");
+    mongoose
+    .connect(mongoURI)
+    .then(() => {
+      console.log("Connected to Mongo Successfully");
     })
+    .catch((err) => {
+      console.log(`error: ${err}`);
+    });
 }
 
 module.exports = connectToMongo;
